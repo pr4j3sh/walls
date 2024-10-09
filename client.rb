14211fa4fa3ab9808b710155b89814906573f648
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-10-09 10:49:47 +0530

    changes for other device connection test

--- client.rb
+++ client.rb
@@ -2,18 +2,21 @@ require 'socket'
 
 BUFFER_SIZE = 1024
 
-HOSTNAME = '192.168.29.251'
-# print 'server ip > '
-# HOSTNAME = gets.chomp
+print 'server ip > '
+HOSTNAME = gets.chomp
 PORT = 4321
 
 
+def local_ip
+  Socket.ip_address_list.detect(&:ipv4_private?).ip_address
+end
+
 socket = UDPSocket.new
-socket.bind('0.0.0.0', 0)
+socket.bind(local_ip, 0)
 
 assigned_port = socket.addr[1]
 
-user = "#{HOSTNAME}:#{assigned_port}"
+user = "#{local_ip}:#{assigned_port}"
 
 puts "connected to server @ #{HOSTNAME}:#{PORT} as #{user}"
 
